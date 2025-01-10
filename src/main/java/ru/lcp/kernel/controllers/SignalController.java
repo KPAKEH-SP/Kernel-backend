e3921bc6017fdc741b381e7c7f4d394cb2d7654c
Denis Emrih <den.emrikh@mail.ru>
2025-01-11 01:04:53 +0400

    -= hotfix =-

--- src/main/java/ru/lcp/kernel/controllers/SignalController.java
+++ src/main/java/ru/lcp/kernel/controllers/SignalController.java
@@ -15,18 +15,21 @@ public class SignalController {
     private final SignalService signalService;
 
     @MessageMapping("/webrtc/candidate/user/{username}")
-    public void handleCandidate(SignalMessage message, @DestinationVariable String username) {
-        signalService.handleCandidate(message, username);
+    @SendTo("/topic/webrtc/candidate/user/{username}")
+    public String handleCandidate(SignalMessage message, @DestinationVariable String username) {
+        return signalService.handleCandidate(message, username);
     }
 
     @MessageMapping("/webrtc/offer/user/{username}")
-    public void handleOffer(SignalMessage message, @DestinationVariable String username) {
-        signalService.handleOffer(message, username);
+    @SendTo("/topic/webrtc/offer/user/{username}")
+    public String handleOffer(SignalMessage message, @DestinationVariable String username) {
+        return signalService.handleOffer(message, username);
     }
 
     @MessageMapping("/webrtc/answer/user/{username}")
-    public void handleAnswer(SignalMessage message, @DestinationVariable String username) {
-        signalService.handleAnswer(message, username);
+    @SendTo("/topic/webrtc/answer/user/{username}")
+    public String handleAnswer(SignalMessage message, @DestinationVariable String username) {
+        return signalService.handleAnswer(message, username);
     }
 
     @MessageMapping("/call/user/{username}")
--- src/main/java/ru/lcp/kernel/services/SignalService.java
+++ src/main/java/ru/lcp/kernel/services/SignalService.java
@@ -2,7 +2,6 @@ package ru.lcp.kernel.services;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.lcp.kernel.dtos.*;
@@ -15,17 +14,15 @@ import ru.lcp.kernel.utils.UserUtils;
 @Service
 @RequiredArgsConstructor
 public class SignalService {
-    private final SimpMessagingTemplate simpMessagingTemplate;
     private final UserUtils userUtils;
 
     @Transactional
-    public void handleCandidate(SignalMessage message, String respondentUsername) {
+    public String handleCandidate(SignalMessage message, String respondentUsername) {
         System.out.println(respondentUsername);
         try {
             User initiator = userUtils.getByToken(message.getSenderToken());
 
-            simpMessagingTemplate.convertAndSend("/topic/webrtc/candidate/user/" + respondentUsername, message.getData());
-            log.debug("\n>>> Sent ICE candidate: {}\n>>>To user: {}", message.getData(), respondentUsername);
+            return message.getData();
         } catch (UserNotFound e) {
             log.warn(e.getMessage());
             throw new RuntimeException(e);
@@ -33,13 +30,11 @@ public class SignalService {
     }
 
     @Transactional
-    public void handleOffer(SignalMessage message, String respondentUsername) {
-
+    public String handleOffer(SignalMessage message, String respondentUsername) {
         try {
             User user = userUtils.getByToken(message.getSenderToken());
 
-            simpMessagingTemplate.convertAndSend("/topic/webrtc/offer/user/" + respondentUsername, message.getData());
-            log.debug("\n>>> Sent offer: {}\n>>>To user: {}", message.getData(), respondentUsername);
+            return message.getData();
         } catch (UserNotFound e) {
             log.warn(e.getMessage());
             throw new RuntimeException(e);
@@ -47,11 +42,11 @@ public class SignalService {
     }
 
     @Transactional
-    public void handleAnswer(SignalMessage message, String respondentUsername) {
+    public String handleAnswer(SignalMessage message, String respondentUsername) {
         try {
             User sender = userUtils.getByToken(message.getSenderToken());
-            simpMessagingTemplate.convertAndSend("/topic/webrtc/answer/user/" + respondentUsername, message.getData());
-            log.debug("\n>>> Sent answer: {}\n>>>To user: {}", message.getData(), respondentUsername);
+
+            return message.getData();
         } catch (UserNotFound e) {
             log.warn(e.getMessage());
             throw new RuntimeException(e);
