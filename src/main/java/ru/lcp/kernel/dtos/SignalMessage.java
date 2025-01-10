ee367c67d329262a8cf131a08562d12355b9eab6
Denis Emrih <den.emrikh@mail.ru>
2025-01-10 22:06:49 +0400

    -= changes =- - Теперь все эндопинты SignalController получают сообщение одного типа - Обработки всех эндпоинтов SignalController происходят не зависимо от чатов

--- src/main/java/ru/lcp/kernel/dtos/SignalMessage.java
+++ src/main/java/ru/lcp/kernel/dtos/SignalMessage.java
@@ -2,11 +2,8 @@ package ru.lcp.kernel.dtos;
 
 import lombok.Data;
 
-import java.util.UUID;
-
 @Data
 public class SignalMessage {
-    private UUID chatId;
-    private String initiatorToken;
+    private String senderToken;
     private String data;
 }
